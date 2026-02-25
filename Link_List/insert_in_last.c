533ca37dd19f46d31521085b72923908d681b6c0
Saurabh Pandey <sp8355063538@gmail.com>
2026-02-25 21:47:18 +0530

    Add linked list insert_in_last C program
    Add a new C implementation for a singly linked list with interactive creation, traversal, and insertion at the end (create, traverse, ins_last). The program uses dynamic allocation for nodes and maintains a head pointer; it prompts the user to build the initial list and optionally append a value at the end. Also include the compiled Windows executable insert_in_last.exe.

---
+++ Link_List/insert_in_last.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int info;
+	struct node *next;
+};
+
+struct node *head=NULL,*newnode,*temp;
+void create();
+void traverse();
+void ins_last();
+
+
+int main(){
+	create();
+	traverse();
+	int n;
+	printf("\n wants to insert \n in last press '1'\n  otherwise '0' :-");
+	printf("");
+	printf("");
+	scanf("%d",&n);
+	if(n==1){
+		ins_last();	
+		traverse();
+	}
+	else{
+		printf("ok");
+		traverse();
+	}
+}
+
+void create(){
+	int choice = 1;
+	
+	while(choice!=0){
+		newnode = (struct node *)malloc(sizeof(struct node));
+		printf("Enter the value :-  ");
+		scanf("%d",&newnode->info);
+		newnode->next = NULL;
+		
+		if(head == NULL){
+			head = temp = newnode;
+		}	
+		else{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		printf("want to insert new value press 1 otherwise 0 :- ");
+		scanf("%d",&choice);
+	}
+}
+
+void ins_last(){
+	temp=head;
+	printf("enter the value");
+	newnode=(struct node *)malloc(sizeof(struct node));
+	scanf("%d",&newnode->info);
+	newnode->next=NULL;
+	if(temp==NULL){
+		head=temp=newnode;
+	}
+	else{
+		while(temp->next!=NULL){
+			temp=temp->next;
+		}
+		temp->next=newnode;	
+	}
+}
+
+void traverse(){
+	temp=head;
+	while(temp != NULL){
+		printf("%d ",temp->info);
+		temp= temp->next;
+	}
+}
