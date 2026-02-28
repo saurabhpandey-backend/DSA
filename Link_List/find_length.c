d98220421f91870ebe945e854192cb2f3b8a4221
Saurabh Pandey <sp8355063538@gmail.com>
2026-02-28 16:03:05 +0530

    Add linked list length program
    Add a C program (find_length.c) that interactively builds a singly linked list, traverses and prints its elements, and computes the list length via find_len(). Also include the compiled binary find_length.exe. The source exposes create(), traverse(), and find_len() functions for basic list operations and user-driven input.

---
+++ Link_List/find_length.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int info;
+	struct node *next;
+};
+
+struct node *head=NULL,*newnode,*temp;
+void create();
+void traverse();
+void find_len();
+
+
+int main(){
+	create();
+	traverse();
+	int n;
+	printf("\n Find length press '1'\n otherwise '0' :- ");
+	scanf("%d",&n);
+	if(n==1){
+		find_len();
+		traverse();
+	}
+	else{
+		printf("ok");
+		traverse();
+	}
+}
+
+void create(){
+
+	int choice = 1;
+	
+	while(choice!=0){
+		newnode = (struct node *)malloc(sizeof(struct node));
+		printf("Enter the value :-  ");
+		scanf("%d",&newnode->info);
+		newnode->next = NULL;
+		
+		if(head == NULL){
+			head = temp = newnode;
+		}	
+		else{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		printf("want to insert new value press 1 otherwise 0 :- ");
+		scanf("%d",&choice);
+	}
+}
+
+void find_len(){
+	int len=1;
+	temp=head;
+	while(temp->next!=NULL){
+		temp=temp->next;
+		len++;
+	}
+	printf("total no. of node and lenth is %d\n",len);
+}
+
+
+void traverse(){
+	temp=head;
+	while(temp != NULL){
+		printf("%d ",temp->info);
+		temp= temp->next;
+	}
+}
