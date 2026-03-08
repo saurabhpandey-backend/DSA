7cd16e94df9ed3765abcb9bb116b7bdbc52c8197
Saurabh Pandey <sp8355063538@gmail.com>
2026-03-08 19:12:11 +0530

    feat: implement circular linked list creation and traversal
    Added two functions in C:
    
    1. create() - to dynamically create nodes of a circular linked list.
       - First node becomes head.
       - Subsequent nodes are linked and last node points back to head.
    
    2. traverse() - to display all nodes in the circular linked list.
    
    This allows storing data in a looped structure and iterating over all nodes.

---
+++ single_circular_Link_list/create_circular_link.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;	
+};
+
+struct node *head=NULL,*newnode,*temp,*tail;
+
+void create();
+void traverse();
+
+int main(){
+	create();
+	traverse();
+}
+
+void create(){
+	int choice=1;
+	while(choice!=0){
+		newnode=(struct node *)malloc(sizeof(struct node));
+		printf("Enter the value :- ");
+		scanf("%d",&newnode->data);
+		newnode->next=NULL;
+		if(head==NULL){
+			head=temp=newnode;
+		}
+		else{
+			temp->next=newnode;
+			newnode->next=head;
+			temp=tail=newnode;
+		}
+		printf("press 1 for newnode :- ");
+		scanf("%d",&choice);
+	}	
+}
+
+void traverse(){
+	temp=head;
+	while(head!=temp->next){
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+	printf("%d ",temp->data);
+}
